eb8926fcdfd0613179c6fa9456d9aa35e2eaa1c5
Michael Hofweller <michael.hofweller@gmail.com>
2023-01-13 13:10:58 -0800

    Expense components sported

--- course-work/section-3-react-basics/src/components/Expenses/ExpenseItem.js
+++ course-work/section-3-react-basics/src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import ExpenseDate from "./ExpenseDate";
-import Card from "./UI/Card";
-import ".ExpenseItem.css";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
 const ExpenseItem = ({date, title, price}) => {
 
